4a964371a932b1bf695415d59a8c72c2d0de3226
youpy <youpy@buycheapviagraonlinenow.com>
2012-04-18 22:24:38 +0900

    add specs

--- spec/option_parser_spec.rb
+++ spec/option_parser_spec.rb
@@ -8,15 +8,40 @@ describe CLI::OptionParser do
     @opts = {
       :env => []
     }
-
-    @agent = CLI::OptionParser.new(opts, @argv).agent
-    @plist = agent2plist(@agent)
   end
 
   shared_examples_for 'valid agent' do
     it "should have necessary attributes" do
-      @plist['Label'].should eql('com.buycheapviagraonlinenow.ruby__foo_rb')
-      @plist['ProgramArguments'].should eql(@argv)
+      agent = CLI::OptionParser.new(opts, @argv).agent
+      plist = agent2plist(agent)
+
+      plist['Label'].should eql('com.buycheapviagraonlinenow.ruby__foo_rb')
+      plist['ProgramArguments'].should eql(@argv)
+    end
+  end
+
+  describe 'empty argv' do
+    let(:opts) do
+      @opts.merge(
+        :daemon => true)
+    end
+
+    it 'should raise if argv is empty' do
+      lambda {
+        CLI::OptionParser.new(opts, []).agent
+      }.should raise_error('full command must be supplied')
+    end
+  end
+
+  describe 'empty options' do
+    let(:opts) do
+      @opts
+    end
+
+    it 'should raise if option is empty' do
+      lambda {
+        CLI::OptionParser.new(opts, @argv).agent
+      }.should raise_error('at least one of --daemon and --interval must be set')
     end
   end
 
@@ -29,10 +54,13 @@ describe CLI::OptionParser do
     it_should_behave_like 'valid agent'
 
     it 'should parse daemon option' do
-      @plist['KeepAlive'].should eql({
+      agent = CLI::OptionParser.new(opts, @argv).agent
+      plist = agent2plist(agent)
+
+      plist['KeepAlive'].should eql({
           'SuccessfulExit' => false
         })
-      @plist['RunAtLoad'].should be_true
+      plist['RunAtLoad'].should be_true
     end
   end
 
@@ -46,7 +74,10 @@ describe CLI::OptionParser do
     it_should_behave_like 'valid agent'
 
     it 'should parse wdir option' do
-      @plist['WorkingDirectory'].should eql('/foo/bar')
+      agent = CLI::OptionParser.new(opts, @argv).agent
+      plist = agent2plist(agent)
+
+      plist['WorkingDirectory'].should eql('/foo/bar')
     end
   end
 
@@ -60,7 +91,10 @@ describe CLI::OptionParser do
     it_should_behave_like 'valid agent'
 
     it 'should parse env option' do
-      @plist['EnvironmentVariables'].should eql({ 'FOO' => 'BAR', 'BAR' => 'BAZ' })
+      agent = CLI::OptionParser.new(opts, @argv).agent
+      plist = agent2plist(agent)
+
+      plist['EnvironmentVariables'].should eql({ 'FOO' => 'BAR', 'BAR' => 'BAZ' })
     end
   end
 
@@ -73,7 +107,10 @@ describe CLI::OptionParser do
     it_should_behave_like 'valid agent'
 
     it 'should parse env option' do
-      @plist['StartInterval'].should eql(120)
+      agent = CLI::OptionParser.new(opts, @argv).agent
+      plist = agent2plist(agent)
+
+      plist['StartInterval'].should eql(120)
     end
   end
 
